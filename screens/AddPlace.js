ffe33b3901267506db6357989ee26d58f10f5038
Khayal Mammadzada <khayalmammadzada@Khayals-MacBook-Air.local>
2023-03-06 23:21:30 +0400

    first commit

---
+++ screens/AddPlace.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import PlaceForm from "../components/Places/PlaceForm";
+import { insertPlace } from "../util/database";
+
+export default function AddPlace({ navigation }) {
+
+  async function createPlaceHandler(place) {
+    await insertPlace(place);
+    navigation.navigate("AllPlaces");
+  }
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
+}
